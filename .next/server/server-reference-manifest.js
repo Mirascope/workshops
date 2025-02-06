047456d629559d12c175a911aefbcf5902c63281
William Bakst <william@mirascope.io>
2025-02-05 19:10:03 -0800

    chore: remove .next

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"vMOG3KDbkFhHIrjhlzvNj5AXkRWAwDGdC2iLXxfc5P4=\"\n}"
--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,18 @@
 import nextra from 'nextra'
  
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true // mandatory, otherwise won't export
+  },
+  // Optional: Change the output directory `out` -> `dist`
+  distDir: "build"
+}
 const withNextra = nextra({
-  // ... Other Nextra config options
+  // ... other Nextra config options
 })
  
-// You can include other Next.js configuration options here, in addition to Nextra settings:
-export default withNextra({
-  // ... Other Next.js config options
-})
+export default withNextra(nextConfig)
